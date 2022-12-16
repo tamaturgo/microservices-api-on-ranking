7a54cd029f7c369a389af3e4f71e41181dca70ba
tamaturgo <ddst.snf20@uea.edu.br>
2022-12-16 09:30:52 -0400

    Mongo DB Connection and Refactor Players Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    JogadoresModule,
+    MongooseModule.forRoot(
+      "mongodb+srv://tamaturgo:diogeles19@cluster0.nsws3z1.mongodb.net/onranking?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
